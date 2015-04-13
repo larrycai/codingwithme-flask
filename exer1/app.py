271d7b2a40ab88f37a2eb1dfca65473186405136
Larry Cai <larry.caiyu@gmail.com>
2015-04-13 21:17:12 +0800

    add debug=True in first exer

--- exer1/app.py
+++ exer1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello HELLO!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
